329b255f77edff488cbaa9d6c61c531d15db7a96
Rawaa-Ahmed-dotcom <rawaaahmed987@gmail.com>
2025-01-31 15:55:36 +0200

    Modified To-Do List

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
+let form = document.querySelector("form");
+let textInput = document.getElementById("text");
+let submitInput = document.getElementById("create");
+let itemsContainer = document.getElementById("list");
 
-let tInput = document.querySelector("[type = 'text']");
-let form = document.forms[0];
-let sInput = document.querySelector("[type = 'submit']");
-let results = document.querySelector(".results");
 
-form.onsubmit = function(event) {
-        event.preventDefault();
-        let newElement = document.createElement("div");
-        let newText = document.createTextNode(tInput.value);
-        // newElement.setAttribute("class","box");
-        // newElement.setAttribute("title","Element");
-        // newElement.setAttribute("id",`id-${i}`);
-        newElement.appendChild(newText);
-        newElement.style.cssText = "background-color: #F5F5F5;padding: 0.5em 1em;"
-        results.appendChild(newElement);
-        
+if(form && textInput && submitInput ) {
+        form.onsubmit = function(event) {
+                event.preventDefault();
+                if(textInput.value.trim() !== "") {
+                        let item = document.createElement("li");
+                        window.localStorage.setItem("text",textInput.value.trim());
+                        item.innerHTML = localStorage.getItem("text");
+                        let icon = document.createElement("i");
+                        icon.setAttribute("class", "fa-solid fa-circle-xmark");
+                        item.appendChild(icon);
+                        itemsContainer.appendChild(item);
+                        textInput.value = "";
+                }
 }
-
-
-
+}
+itemsContainer.addEventListener("click",function(element) {
+        if(element.target.tagName === "LI"){
+                element.target.classList.toggle("checked");
+        }
+        else if(element.target.tagName === "I"){
+                element.target.parentElement.remove();
+        }                     
+});
